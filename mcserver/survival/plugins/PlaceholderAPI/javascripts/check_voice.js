d4d3c40c9ad11fbc232effb650b3388d565b50ce
Laika1111 <khanisornx24@gmail.com>
2022-11-27 16:51:01 +0700

    fix papi

--- mcserver/survival/plugins/PlaceholderAPI/javascripts/check_voice.js
+++ mcserver/survival/plugins/PlaceholderAPI/javascripts/check_voice.js
@@ -2,7 +2,7 @@ var placeholder = "%voicechat_prefix_uses_voicechat%";
 var voice = PlaceholderAPI.static.setPlaceholders(BukkitPlayer, placeholder);
 
 var checkVoice = function (voice) {
-  return voice === "§aY" ? "&a∙" : "&f";
+  return voice === "§aY" ? "voiceuse" : "voicenotuse";
 };
 
 checkVoice(voice);
